bc5284cae821f98c9c19834a0b8e5301b9068284
uncleclam <camerondpatterson@gmail.com>
2023-04-14 22:48:31 -0700

    scripts

---
+++ scripts/curl_to_s3.sh
@@ -0,0 +1,10 @@
+
+#!/bin/sh
+
+ curl -L --get https://api.serpwow.com/search \
+    -d api_key="6521809044384F6486918BFCB59AF83D" \
+    -d q="farts" \
+    -d engine="google" \
+    -d page="1" \
+    -d num="10" \
+        -o  output_1.txt
