0b0e4e2f507787c404c1528b390749c528c69f0f
uncleclam <camerondpatterson@gmail.com>
2023-04-21 14:49:13 -0700

    No More Paint

--- scripts/boom.php
+++ scripts/boom.php
@@ -1,5 +1,5 @@
 #!/usr/bin/env php
 
 <?php
-shell_exec("(sh -x ./load_urine.sh 2>&1 | tee -a file.log)");
+shell_exec("(sh -x ./load_wee.sh 2>&1 | tee -a file.log)");
 ?>
--- scripts/python/weeing.py
+++ scripts/python/weeing.py
@@ -1,15 +1,19 @@
 from datetime import datetime
 import json
+import os
 import pandas as pd
 import requests
 
 # read json file
-with open('../urination.json', 'r') as data_file:
+with open('./urination.json', 'r') as data_file:
     json_data = data_file.read()
 data = json.loads(json_data)["related_searches"]
 
 # create DataFrame from json data
 df = pd.DataFrame(data)
+
+# create directory for query results
+os.makedirs("results",exist_ok=True)
 
 # extract query value
 for idx, row in df.iterrows():
@@ -23,6 +27,7 @@ for idx, row in df.iterrows():
         "google_domain": "google.com"
     }
     result = requests.get(url,params=payload)
-    print(result.json)
-
+    print(result.json())
+    with open(f"results/related_searches_{idx}.json","w") as file:
+        json.dump(result.json(),file)
 
